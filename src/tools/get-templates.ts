ea5d28e8e8188b42c6d47971842a7aac433a2577
luofangchao <luofangchao@pcitech.com>
2024-01-18 22:52:53 +0800

    repo init

---
+++ src/tools/get-templates.ts
@@ -0,0 +1,48 @@
+import path from 'path'
+import fs from 'fs'
+import vscode from 'vscode'
+
+import { WORKSPACE_PATH, TEMPLATE_FILE_NAME, CONFIG_GROUP, config, requireModule, localize } from '../tools'
+
+export interface TemplateParserParams {
+  defaultName: string
+}
+
+export interface TemplateBaseType {
+  namespace?: (params: TreeInterface) => string
+  params?: (params: TreeInterface) => string
+  paramsItem?: (item: TreeInterfacePropertiesItem, params: TreeInterface) => string
+  response?: (params: TreeInterface) => string
+  responseItem?: (item: TreeInterfacePropertiesItem, params: TreeInterface) => string
+  copyRequest?: (params: FileHeaderInfo) => string | string[]
+}
+
+export let templateConfig: TemplateBaseType = {}
+
+/** 获取工作区模板配置 */
+export function getWorkspaceTemplateConfig(): TemplateBaseType {
+  const workspaceConfigPath = getWorkspaceTemplatePath()
+  if (fs.existsSync(workspaceConfigPath)) {
+    templateConfig = requireModule(workspaceConfigPath)
+  }
+  console.log('templateConfig===>', templateConfig);
+  if (templateConfig.copyRequest) {
+    vscode.commands.executeCommand('setContext', `${CONFIG_GROUP}.hasCopyRequestFn`, 1)
+  } else {
+    vscode.commands.executeCommand('setContext', `${CONFIG_GROUP}.hasCopyRequestFn`, 0)
+  }
+
+  return templateConfig
+}
+
+export function getWorkspaceTemplatePath() {
+  const workspaceConfigFileName = path.join(WORKSPACE_PATH || '', '.vscode', TEMPLATE_FILE_NAME)
+
+  if (fs.existsSync(`${workspaceConfigFileName}.js`)) {
+    return `${workspaceConfigFileName}.js`
+  } else {
+    return `${workspaceConfigFileName}.cjs`
+  }
+}
+
+getWorkspaceTemplateConfig()
---
+++ src/tools/io.ts
@@ -0,0 +1,68 @@
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+
+import { WORKSPACE_PATH, log, config } from '.'
+
+/**
+ * 递归创建路径
+ * @param dir
+ * @param inputPath
+ * @param split
+ */
+export function mkdirRecursive(dir: string, inputPath = WORKSPACE_PATH || '', split = '/') {
+  const dirArr = dir.split(split)
+  const dir2 = dirArr.reduce((dirPath, folder) => {
+    const p1 = path.join(inputPath, dirPath)
+    if (!fs.existsSync(p1)) fs.mkdirSync(p1)
+    return dirPath + '/' + folder
+  })
+  const p2 = path.join(inputPath, dir2)
+  if (!fs.existsSync(p2)) fs.mkdirSync(p2)
+}
+
+/**
+ * 动态导入一个 JS 文件
+ * @param modulePath 要导入的文件路径
+ * @param clearCache 是否清除缓存
+ */
+export function requireModule(modulePath: string, clearCache = true) {
+  try {
+    const m = require(modulePath)
+    if (clearCache) {
+      setTimeout(() => {
+        delete require.cache[require.resolve(modulePath)]
+      }, 200)
+    }
+    return m
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
+
+/**
+ * 保存文件
+ * @param docStr
+ * @param filePath
+ */
+export async function saveDocument(docStr: string, filePath: string, fileHeaderDocStr?: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filePath)) {
+      // const dir = path.dirname(filePath)
+      // fs.mkdirSync(dir, { recursive: true })
+      /** 创建文件并写入顶部自定义内容，如import导入模块、函数等等 */ 
+      if (fileHeaderDocStr) {
+        fs.writeFileSync(filePath, fileHeaderDocStr + os.EOL, 'utf-8')
+      }
+    }
+    
+    /** 追加内容到文件末尾 */
+    try {
+      fs.appendFileSync(filePath, docStr + os.EOL, 'utf-8')
+      resolve(void 0)
+    } catch (error: any) {
+      log.error(error, true)
+      reject()
+    }
+  })
+}
